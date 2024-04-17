bf585748ba58c543447587ef49e2edd410ac2688
ElguetaM <vivianaandrea09@hotmail.com>
2024-04-16 20:24:26 -0400

    Prueba completa

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const __dirname = import.meta.dirname;
 app.use(express.static(path.join(__dirname, "assets")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("assets"));
 app.use(bancoRoutes);
 
 app.listen(PORT, () =>
